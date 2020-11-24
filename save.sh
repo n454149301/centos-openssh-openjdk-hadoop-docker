8c35c692114c33438db85f019cbee49be20010c3
duan <naonao800@163.com>
2020-11-24 22:45:33 +0800

    添加ansible集群启动脚本，集群测试通过

--- save.sh
+++ save.sh
@@ -3,4 +3,4 @@
 rm -rfd n454149301-centos-openssh-openjdk-hadoop-3.2.1.tar
 rm -rfd n454149301-centos-openssh-openjdk-hadoop-3.2.1.tar.xz
 sudo docker save "n454149301/centos-openssh-openjdk-hadoop:3.2.1" -o n454149301-centos-openssh-openjdk-hadoop-3.2.1.tar
-sudo xz n454149301-centos-openssh-openjdk-hadoop-3.2.1.tar
+sudo xz -v n454149301-centos-openssh-openjdk-hadoop-3.2.1.tar
